80b5c3b86f79450bf4a2793734f9e8b7201b4fd0
jonas-45 <jonaswullah@gmail.com>
2023-02-22 20:02:08 +0000

    Display calculation results

--- src/components/DisplayAnswer.js
+++ src/components/DisplayAnswer.js
@@ -1,11 +1,31 @@
 import '../index.css';
+import PropTypes from 'prop-types';
 
-function DisplayAnswer() {
+function DisplayAnswer(props) {
+  console.log('dataType: ', props.data);
+  const { total, next } = props.data;
+  console.log('newval1: ', total, ' newval2: ', next);
+  const displayVal = (value1, value2) => {
+    console.log('val1: ', value1, ' val2: ', value2);
+
+    if (value2 !== null || value2 === '') {
+      console.log('displaying value 2');
+      return value2;
+    }
+    return value1;
+  };
   return (
     <div className="answer-display">
-      0
+      { displayVal(total, next) }
     </div>
   );
 }
+
+DisplayAnswer.propTypes = {
+  data: PropTypes.shape({
+    total: PropTypes.string,
+    next: PropTypes.string,
+  }).isRequired,
+};
 
 export default DisplayAnswer;
