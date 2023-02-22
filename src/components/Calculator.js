3fa97816d86f254fc591dd2e331ef0af394a4007
jonas-45 <jonaswullah@gmail.com>
2023-02-22 19:58:21 +0000

    Add useState hook and click handler function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,20 @@
-import DisplayAnswer from './DisplayAnswer';
+import React, { useState } from 'react';
 import DisplayCalculatorKeys from './DisplayCalculatorKeys';
+import calculate from '../logic/calculate';
+import DisplayAnswer from './DisplayAnswer';
 
 function Calculator() {
+  const [calobj, setCalculation] = useState({ total: '0', next: '0', operation: '' });
+
+  const clickHandler = (buttonName) => {
+    const result = calculate(calobj, buttonName);
+    console.log('RESULT: ', result);
+    setCalculation(result);
+  };
   return (
     <div className="calculator-container">
-      <DisplayAnswer />
-      <DisplayCalculatorKeys />
+      <DisplayAnswer data={calobj} />
+      <DisplayCalculatorKeys performCalculation={clickHandler} />
     </div>
   );
 }
