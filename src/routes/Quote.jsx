6df1bef3a4856004ee2f2e175d754571830e9a5b
jonas-45 <jonaswullah@gmail.com>
2023-03-01 20:31:38 +0000

    Add quote content and styles

--- src/routes/Quote.jsx
+++ src/routes/Quote.jsx
@@ -1,6 +1,6 @@
 const Quote = () => (
-  <div>
-    <p>
+  <div className="quote-page">
+    <p className="quote-text">
       Mathematics is not about numbers, equations, computations, or
       algorithms: it is about understanding. - William Paul Thurston.
     </p>
